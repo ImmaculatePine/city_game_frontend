6fb9ceefbe86a7d712a7f586f96f1798698abb81
Alexander Borovykh <immaculate.pine@gmail.com>
2018-06-21 22:10:43 +0200

    Add autosuggestions for waypoints

--- src/components/new-waypoint.js
+++ src/components/new-waypoint.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import Autosuggest from 'react-autosuggest'
+import classNames from 'classnames'
 
 export default class NewWaypoint extends Component {
   static propTypes = {
@@ -12,16 +14,97 @@ export default class NewWaypoint extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { placeId: '' }
+    this.state = {
+      placeId: '',
+      placeName: '',
+      suggestions: []
+    }
 
-    this.handleInputChange = this.handleInputChange.bind(this)
+    this.getSuggestions = this.getSuggestions.bind(this)
+    this.getSuggestionValue = this.getSuggestionValue.bind(this)
+    this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(
+      this
+    )
+    this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(
+      this
+    )
+    this.onSuggestionSelected = this.onSuggestionSelected.bind(this)
+    this.onPlaceChange = this.onPlaceChange.bind(this)
+    this.onPlaceBlur = this.onPlaceBlur.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleInputChange({ target: { name, value } }) {
+  getSuggestions(value) {
+    const inputValue = value.trim().toLowerCase()
+    const inputLength = inputValue.length
+    const { places, game } = this.props
+    const waypoints = game.waypoints || []
+    const usedPlaceIds = waypoints.map(waypoint => waypoint.place.id)
+    const notUsedPlaces = places.filter(
+      place => !usedPlaceIds.includes(place.id)
+    )
+
+    return inputLength === 0
+      ? []
+      : notUsedPlaces.filter(
+          place => place.name.toLowerCase().slice(0, inputLength) === inputValue
+        )
+  }
+
+  getSuggestionValue(suggestion) {
+    return suggestion.name
+  }
+
+  onSuggestionsFetchRequested({ value }) {
     this.setState({
-      [name]: value
+      suggestions: this.getSuggestions(value)
     })
+  }
+
+  onSuggestionsClearRequested() {
+    this.setState({
+      suggestions: []
+    })
+  }
+
+  onSuggestionSelected(
+    _event,
+    {
+      suggestion: { id, name }
+    }
+  ) {
+    this.setState({
+      placeId: id,
+      placeName: name
+    })
+  }
+
+  renderSuggestion(suggestion, { isHighlighted }) {
+    const classes = classNames('card', {
+      'has-background-white-ter': isHighlighted
+    })
+
+    return (
+      <div className={classes}>
+        {suggestion.name} <small>{suggestion.address}</small>
+      </div>
+    )
+  }
+
+  onPlaceChange(_event, { newValue }) {
+    this.setState({
+      placeName: newValue
+    })
+  }
+
+  onPlaceBlur(_event, { highlightedSuggestion }) {
+    if (highlightedSuggestion) {
+      const { id, name } = highlightedSuggestion
+      this.setState({
+        placeId: id,
+        placeName: name
+      })
+    }
   }
 
   handleSubmit(event) {
@@ -32,6 +115,10 @@ export default class NewWaypoint extends Component {
     const positions = (game.waypoints || []).map(waypoint => waypoint.position)
     const position = Math.max(-1, ...positions) + 1
     onSubmit(gameId, { placeId, position })
+    this.setState({
+      placeId: '',
+      placeName: ''
+    })
   }
 
   render() {
@@ -44,12 +131,8 @@ export default class NewWaypoint extends Component {
   }
 
   _renderModal() {
-    const { places, game } = this.props
-    const waypoints = game.waypoints || []
-    const usedPlaceIds = waypoints.map(waypoint => waypoint.place.id)
-    const notUsedPlaces = places.filter(
-      place => !usedPlaceIds.includes(place.id)
-    )
+    const { placeName, suggestions } = this.state
+    const { game } = this.props
 
     return (
       <div className="modal is-active">
@@ -63,18 +146,25 @@ export default class NewWaypoint extends Component {
             <section className="modal-card-body">
               <div className="field">
                 <div className="control">
-                  <select
-                    name="placeId"
-                    value={this.state.placeId}
-                    onChange={this.handleInputChange}
-                  >
-                    <option value="" />
-                    {notUsedPlaces.map(place => (
-                      <option key={place.id} value={place.id}>
-                        {place.name}
-                      </option>
-                    ))}
-                  </select>
+                  <Autosuggest
+                    suggestions={suggestions}
+                    onSuggestionsFetchRequested={
+                      this.onSuggestionsFetchRequested
+                    }
+                    onSuggestionsClearRequested={
+                      this.onSuggestionsClearRequested
+                    }
+                    onSuggestionSelected={this.onSuggestionSelected}
+                    getSuggestionValue={this.getSuggestionValue}
+                    renderSuggestion={this.renderSuggestion}
+                    inputProps={{
+                      className: 'input',
+                      value: placeName,
+                      onChange: this.onPlaceChange,
+                      onBlur: this.onPlaceBlur,
+                      autoFocus: true
+                    }}
+                  />
                 </div>
               </div>
             </section>
