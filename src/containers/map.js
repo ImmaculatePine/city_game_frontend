56baa51d23935a1157ee93423032d3a5c926e7ec
Alexander Borovykh <immaculate.pine@gmail.com>
2018-06-23 16:53:12 +0200

    Update map height

--- src/containers/map.js
+++ src/containers/map.js
@@ -18,9 +18,9 @@ export class MapContainer extends Component {
         {...this.props}
         isMarkerShown={false}
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=directions&key=${GOOGLE_MAPS_API_KEY}`}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
+        loadingElement={<div style={{ height: '100%' }} />}
+        containerElement={<div style={{ height: '100%' }} />}
+        mapElement={<div style={{ height: '100%' }} />}
       />
     )
   }
